e8ff34cacc1366bd19d1ce02467e02580f0d5dbd
Valentin Schiaffino <valenschg@gmail.com>
2019-05-15 20:23:36 -0300

    Hice que el programa ej1.py(el de repetir la palabra mil veces), printee si es el programa principal, pero si está siendo testeado que devuelva, esto lo hice para poder evaluar si funciona bien la funcion.

--- tp3/src/ej1.py
+++ tp3/src/ej1.py
@@ -5,10 +5,17 @@ def repetirPalabra(palabra):
     Recibe un solo parámetro, que es la palabra a repetir mil veces.
 
     La función no devuelve ningún valor sinó que lo imprime en pantalla."""
-    print((palabra + " ") * 1000)
+    palabra_mil_veces = (palabra + " ") * 1000
+    if __name__ == "__main__":
+        print(palabra_mil_veces)
+    else:
+        return palabra_mil_veces
 
-# Pidiendo al usuario que ingrese la palabra a repetir.
-palabraPedida = input("Ingrese la palabra a repetir: ")
-# Usando la función para repetir la palabra, con el parámetro que le pedí
-# anteriormente al usuario.
-repetirPalabra(palabraPedida)
+
+# Si estoy ejecutando el programa como principal entonces que pida
+if  __name__ == "__main__":
+    # Pidiendo al usuario que ingrese la palabra a repetir.
+    palabraPedida = input("Ingrese la palabra a repetir: ")
+    # Usando la función para repetir la palabra, con el parámetro que le pedí
+    # anteriormente al usuario.
+    repetirPalabra(palabraPedida)
