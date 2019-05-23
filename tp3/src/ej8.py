6d32f1d9f1ec2609c9f99e48f116a2de9935b6d9
Valentin Schiaffino <valenschg@gmail.com>
2019-05-23 19:35:01 -0300

    Acondicioné la función para que cumpla snake_case

--- tp3/src/ej8.py
+++ tp3/src/ej8.py
@@ -1,6 +1,4 @@
-
-
-def anioEnNumerosRomanos(anio):
+def anio_en_numeros_romanos(anio):
     """Función que convierte números de tipo enteros a números romanos.
 
     La función recibe un solo parámetro, debe ser el número a convertir en
@@ -15,11 +13,11 @@ def anioEnNumerosRomanos(anio):
     # Empiezo checkeando si hay algun error en el año dado.
     # Si no es int lo informo y salgo de la función.
     if not isinstance(anio, int):
-        return "Error. Se ingresó un tipo de dato no soportado."
+        return None
     # Si no está en el rango 0 a 1 000 000 entonces lo informo y salgo de la
     # función
     if not (anio >= 0 and anio <= 1000000):
-        return "Error. Se ingresó un valor fuera de las cotas(0-1 000 000)."
+        return None
     # Ta todo ok entonces sigo
 
     # Bueno, acá estoy creando muchas tuplas, para poder reemplazar los valores
@@ -29,7 +27,7 @@ def anioEnNumerosRomanos(anio):
     unidad = ("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
     decena = ("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
     centena = ("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
-    unidadDeMil = (
+    unidad_de_mil = (
         "",
         "M",
         "MM",
@@ -40,7 +38,7 @@ def anioEnNumerosRomanos(anio):
         u"V\u0305I\u0305I\u0305",
         u"V\u0305I\u0305I\u0305I\u0305",
         u"I\u0305X\u0305")
-    decenaDeMil = (
+    decena_de_mil = (
         "",
         u"X\u0305",
         u"X\u0305X\u0305",
@@ -51,7 +49,7 @@ def anioEnNumerosRomanos(anio):
         u"L\u0305X\u0305X\u0305",
         u"L\u0305X\u0305X\u0305X\u0305",
         u"X\u0305C\u0305")
-    centenaDeMil = (
+    centena_de_mil = (
         "",
         "C\u0305",
         "C\u0305C\u0305",
@@ -62,19 +60,19 @@ def anioEnNumerosRomanos(anio):
         "D\u0305C\u0305C\u0305",
         "D\u0305C\u0305C\u0305C\u0305",
         "C\u0305M\u0305")
-    unidadDeMillon = ("", u"M\u0305")
+    unidad_de_millon = ("", u"M\u0305")
     # Creo una tupla con las tuplas para poder recorrerlas más fácil cuando
     # use el for.
-    listaDeTuplas = (
+    lista_de_tuplas = (
         unidad,
         decena,
         centena,
-        unidadDeMil,
-        decenaDeMil,
-        centenaDeMil,
-        unidadDeMillon)
+        unidad_de_mil,
+        decena_de_mil,
+        centena_de_mil,
+        unidad_de_millon)
     # Creo una variable para guardar el número final
-    numeroRomanoFinal = ""
+    numero_romano_final = ""
     # Paso el año a string para poder operar bien con él.
     anio = str(anio)
     # Creo una variable len con la lengitud del año, ya que lo uso varias
@@ -89,15 +87,15 @@ def anioEnNumerosRomanos(anio):
     for i in range(Len, -1, -1):
         # Creo una variable para guardar la tupla que voy a usar en este bucle
         # y la igualo a la tupla correspondiente.
-        tipoDeCaracter = listaDeTuplas[j]
+        tipo_de_caracter = lista_de_tuplas[j]
 
-        characterActual = int(anio[i - 1])
-        numeroRomanoFinal = tipoDeCaracter[characterActual] + numeroRomanoFinal
+        caracter_actual = int(anio[i - 1])
+        numero_romano_final = tipo_de_caracter[caracter_actual] + numero_romano_final
         if j == Len - 1:
             break
         j += 1
 
-    return numeroRomanoFinal
+    return numero_romano_final
 
 
 #for i in range(0,1000):
