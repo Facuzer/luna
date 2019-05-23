5321621967ae6e9262aca1fceb17031c8bc40dbc
Valentin Schiaffino <valenschg@gmail.com>
2019-05-23 19:05:44 -0300

    Cambie algunos errores de documentación y hice que el programa cumpla con la documentación, y después arreglé algo que me corrigió Fede en la entrega de este tp cambiando mi ecuación con dia_del_año % 7.

--- tp3/src/ej7.py
+++ tp3/src/ej7.py
@@ -1,4 +1,4 @@
-def queDiaEs(diaDelAnio):
+def que_dia_es(dia_del_anio):
     """ Función que, suponiendo que el primer día del año es lunes, imprime el día
     de la semana que es, dado el día del año.
 
@@ -6,24 +6,28 @@ def queDiaEs(diaDelAnio):
     de tipo entero entre 1 y 366.
 
     La función imprime un string y en caso de que no se hayan respetado las
-    cotas o se haya dado un tipo de dato incorrecto, imprimirá en pantalla
-    avisando el error y especificando cuál fue."""
+    cotas o se haya dado un tipo de dato incorrecto, devolverá None."""
     # Me fijo si hay algun error al llamar a la función
     # Si me dieron un diaDelAnio de tipo distinto a int entonces que devuelva
     # error
-    if not isinstance(diaDelAnio, int):
-        print("Error. Se ingreso un tipo de dato no admitido.")
+    if not isinstance(dia_del_anio, int):
+        if __name__ == "__main__":
+            print("Error. Se ingreso un tipo de dato no admitido.")
+        else:
+            return None
     # Si me dieron un diaDelAnio que no esta entre 1 y 366 entonces que me
     # devuelva error
-    if not(diaDelAnio > 0 or diaDelAnio <= 366):
-        print("Error. Se ingreso un valor que no está entre 1 y 366.")
+    if not(dia_del_anio > 0 or dia_del_anio <= 366):
+        if __name__ == "__main__":
+            print("Error. Se ingreso un valor que no está entre 1 y 366.")
+        else:
+            return None
     # Está todo ok entonces empiezo
 
     # Creo una tupla para que cuando sepa que día de la semana toca, luego lo
     # pueda imprimir en letras(Esto lo hice para no tener que hacer un montón
     # de ifs anidados).
-
-    diasEnLetras = (
+    dias_en_letras = (
         "Domingo",
         "Lunes",
         "Martes",
@@ -34,13 +38,14 @@ def queDiaEs(diaDelAnio):
         "Domingo"
     )
     # Me fijo qué dia del año es con una simple ecuación.
-    diaDelAnio = diaDelAnio - (diaDelAnio // 7) * 7
+    # dia_del_anio = dia_del_anio - (dia_del_anio // 7) * 7
+    dia_del_anio = dia_del_anio % 7
     # Paso el día del año que me quedó en números a letras, gracias a la
     # tupla.
-    diaDelAnio = diasEnLetras[diaDelAnio]
+    dia_del_anio = dias_en_letras[dia_del_anio]
     # Imprimo el día del año en pantalla.
-    print(diaDelAnio)
+    print(dia_del_anio)
 
 
 for i in range (1,367):
-    queDiaEs(i)
+    que_dia_es(i)
