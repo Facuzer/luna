8f1dbeb07f776bdc6b986681fdc00fc5efb9f96c
Valentin Schiaffino <valenschg@gmail.com>
2019-05-30 19:10:25 -0300

    Agregué el test de la función comprobar_capitalizacion()

---
+++ tp5/tests/test_ej4.py
@@ -0,0 +1,47 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+from ej3 import comprobar_capitalizacion  # noqa
+
+
+class Test_comprobar_capitalizacion(unittest.TestCase):
+    def test_comprobar_capitalizacion_int(self):
+        self.assertEqual(comprobar_capitalizacion(1, 1), TypeError)
+        self.assertEqual(comprobar_capitalizacion(1000, "diez"), TypeError)
+        self.assertEqual(comprobar_capitalizacion("veinte", 12837283),
+                                                  TypeError)  # noqa
+
+    def test_comprobar_capitalizacion_float(self):
+        self.assertEqual(comprobar_capitalizacion(1.12837, 1.81273), TypeError)
+        self.assertEqual(comprobar_capitalizacion(1000.128, "diez"), TypeError)
+        self.assertEqual(comprobar_capitalizacion("diez", 283.818), TypeError)
+
+    def test_comprobar_capitalizacion_lista(self):
+        self.assertEqual(comprobar_capitalizacion([1, 2], [3, 4]), TypeError)
+        self.assertEqual(comprobar_capitalizacion(["soy", "lista"],
+                                                  ["Yo", "No"]), TypeError)
+
+    def test_comprobar_capitalizacion_tupla(self):
+        self.assertEqual(comprobar_capitalizacion((1, 2), (3, 4)), TypeError)
+        self.assertEqual(comprobar_capitalizacion(("soy", "tupla"),
+                                                  ("Yo no")), TypeError)
+
+    def test_comprobar_capitalizacion_string(self):
+        self.assertEqual(comprobar_capitalizacion("república argentina",
+                                                  "REPÚBLICA ARGENTINA"), True)
+        self.assertEqual(comprobar_capitalizacion("me gusta el queso",
+                                                  "ME GUSTA EL QUESO"), True)
+        self.assertEqual(comprobar_capitalizacion("Me GuSTa EL QuEsO",
+                                                  "ME GUSTA EL QUESO"), True)
+        self.assertEqual(comprobar_capitalizacion("Aguante rappi",
+                                                  "AGUANTE RAPPi"), False)
+        self.assertEqual(comprobar_capitalizacion("Soy valen",
+                                                  "Soy fede"), False)
+        self.assertEqual(comprobar_capitalizacion("Minecraft",
+                                                  "MINECRAft"), False)
+        self.assertEqual(comprobar_capitalizacion("HoLa",
+                                                  "HoLA"), False)
+
+
+unittest.main()
