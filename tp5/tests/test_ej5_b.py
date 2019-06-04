6be2d1645faa919f96f2bbf637fc37de04c03021
Valentin Schiaffino <valenschg@gmail.com>
2019-06-04 19:15:08 -0300

    Hice el test del ej5_b

---
+++ tp5/tests/test_ej5_b.py
@@ -0,0 +1,46 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/'))
+from ej5_b import capitalizar_primera_letra  # noqa
+
+
+class Test_capitalizar_primera_letra(unittest.TestCase):
+    def test_capitalizar_primera_letra_int(self):
+        self.assertEqual(capitalizar_primera_letra(1), TypeError)
+
+    def test_capitalizar_primera_letra_float(self):
+        self.assertEqual(capitalizar_primera_letra(1.12837), TypeError)
+
+    def test_capitalizar_primera_letra_lista(self):
+        self.assertEqual(capitalizar_primera_letra([1, 2, 3, 4]), TypeError)
+        self.assertEqual(capitalizar_primera_letra(["soy", "una", "tupla"]),
+                                               TypeError)
+
+    def test_capitalizar_primera_letra_tupla(self):
+        self.assertEqual(capitalizar_primera_letra((1, 2, 3, 4)), TypeError)
+        self.assertEqual(capitalizar_primera_letra(("soy", "tupla")), TypeError)
+
+    def test_capitalizar_primera_letra_string(self):
+        self.assertEqual(capitalizar_primera_letra("hola todo bien"),
+                                                   "Hola Todo Bien")
+        self.assertEqual(capitalizar_primera_letra("windows media center"),
+                                                   "Windows Media Center")
+        self.assertEqual(capitalizar_primera_letra("Hoy dia Pleno"),
+                                                   "Hoy Dia Pleno")
+        self.assertEqual(capitalizar_primera_letra("sOY mUY feLIZ"),
+                                                   "W")
+        self.assertEqual(capitalizar_primera_letra("valenTIN schiaFFINO"),
+                                                   "ValenTIN SchiaFFINO")
+        self.assertEqual(capitalizar_primera_letra("hola señor"),
+                                                   "Hola Señor")
+        self.assertEqual(capitalizar_primera_letra("Futbol es de enrique"), 
+                                                   "Futbol Es De Enrique")
+        self.assertEqual(capitalizar_primera_letra("Complete the Monument"),
+                                                   "Complete The Monument")
+        self.assertEqual(capitalizar_primera_letra("Hola"),
+                                                   "Hola")
+        self.assertEqual(capitalizar_primera_letra("s"), "S")
+
+
+unittest.main()
