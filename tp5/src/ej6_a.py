c1631e2892aa63deed057fffb990a6f000afbc5e
Valentin Schiaffino <valenschg@gmail.com>
2019-06-05 19:43:06 -0300

    Hice el ej6_a

---
+++ tp5/src/ej6_a.py
@@ -0,0 +1,21 @@
+def solo_consonantes(frase):
+    """
+    Función que recibe una frase, es decir un conjunto de palabras separadas
+    por espacio y devuelve una copia de la frase pero con solo consonantes.
+
+    Recibe por parámetro la frase que debe ser de tipo string, de lo contrario
+    devuelve TypeError.
+
+    La función devuelve un string.
+    """
+    # Validaciones
+    if not isinstance(frase, str):
+        return TypeError
+    # Todo ok
+    frase_final = ""
+    vocales = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U",
+               "á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú"]
+    for letra in frase:
+        if not letra in vocales:
+            frase_final += letra
+    return frase_final
