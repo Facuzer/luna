ce6da2c685374f8eaf07c558d147d3dd84324b44
Valentin Schiaffino <valenschg@gmail.com>
2019-06-05 17:02:36 -0300

    a

---
+++ tp5/src/ej5_b.py
@@ -0,0 +1,27 @@
+def capitalizar_primera_letra(frase):
+    """
+    Función que recibe una frase, es decir un conjunto de palabras separadas
+    por espacio y devuelve la misma frase pero con la primera letra de cada
+    palabra en mayúsculas, sin importar las demás letras de la palabra.
+
+    Recibe por parámetro la frase que debe ser de tipo string, de lo contrario
+    devuelve TypeError.
+
+    La función devuelve un string con la frase, pero con la primera letra de
+    cada palabra en mayúsculas.
+    """
+    # Validaciones
+    if not isinstance(frase, str):
+        return TypeError
+    # Todo ok
+    resultado_final = []
+    # Pongo todas las palabras en una lista
+    palabras = frase.split(" ")
+    # Las recorro y cambio la primera letra de cada palabra
+    for palabra in palabras:
+        # Cambio la primera letra de la palabra
+        palabra = palabra[0].upper() + palabra[1:]
+        resultado_final.append(palabra)
+    # Lo cambio de lista a string
+    resultado_final = " ".join(resultado_final)
+    return resultado_final
