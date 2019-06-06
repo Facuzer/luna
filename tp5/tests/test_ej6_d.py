c3a93904e6bc2e01ac914daf0dc33db57464de87
Valentin Schiaffino <valenschg@gmail.com>
2019-06-06 17:16:29 -0300

    Hice el test_ej6_d

---
+++ tp5/tests/test_ej6_d.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/'))
+from ej6_d import check_palindromo  # noqa
+
+
+class Test_check_palindromo(unittest.TestCase):
+    def test_check_palindromo_int(self):
+        self.assertEqual(check_palindromo(1), TypeError)
+
+    def test_check_palindromo_float(self):
+        self.assertEqual(check_palindromo(1.12837), TypeError)
+
+    def test_check_palindromo_lista(self):
+        self.assertEqual(check_palindromo([1, 2, 3, 4]), TypeError)
+        self.assertEqual(check_palindromo(["soy", "una", "tupla"]),
+                                               TypeError)
+
+    def test_check_palindromo_tupla(self):
+        self.assertEqual(check_palindromo((1, 2, 3, 4)), TypeError)
+        self.assertEqual(check_palindromo(("soy", "tupla")), TypeError)
+
+    def test_check_palindromo_string(self):
+        self.assertEqual(check_palindromo("hola todo bien"), False)
+        self.assertEqual(check_palindromo("anita lava la tina"), True)
+        self.assertEqual(check_palindromo("asdsa"), True)
+        self.assertEqual(check_palindromo("sOY mUY feLIZ"), False)
+        self.assertEqual(check_palindromo("Neuquen"), True)
+        self.assertEqual(check_palindromo("Neuquén"), False)
+        self.assertEqual(check_palindromo(""), True )
+        self.assertEqual(check_palindromo("F"), True)
+        self.assertEqual(check_palindromo("Hola"), False)
+        self.assertEqual(check_palindromo("s"), True)
+
+
+unittest.main()
